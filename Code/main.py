74e6836ff5b83b2b562761555e6b164843633ae3
JustJoshinDev <104283405+JustJoshinDev@users.noreply.github.com>
2024-11-10 22:20:33 +0000

    Updated to v0.0.2

--- Code/main.py
+++ Code/main.py
@@ -1,5 +1,7 @@
 from PySide6.QtWidgets import QFileDialog, QColorDialog, QMainWindow, QApplication
 from PySide6.QtGui import QTextCharFormat, QFont, QTextCursor
+from PySide6.QtCore import QUrl, QFile
+import os.path
 import ui_main
 
 class MyGUI(QMainWindow):  # Main window setup
@@ -8,25 +10,66 @@ class MyGUI(QMainWindow):  # Main window setup
         self.ui = ui_main.Ui_MainWindow()
         self.ui.setupUi(self)
         self.show()
-
+        
+        self.bible_versions = {
+            "American Standard Version": f'{os.path.dirname(os.path.abspath(__file__))}\\.\\db\\asv.html',
+            "King James Version": f'{os.path.dirname(os.path.abspath(__file__))}\\.\\db\\kjv.html',
+        }
+        
         # Initialize default font settings
-        self.ui.textSize.setValue(12)
+        self.bible_size_value = 8
+        self.ui.testSize.setValue(12)
         self.ui.fontSelect.setCurrentFont(QFont('Arial'))
         self.currentFormat = QTextCharFormat()  # Store current format
+
+        # Initialize KJV Bible
+        html_file_path = os.path.abspath(f'{os.path.dirname(os.path.abspath(__file__))}\\.\\db\\kjv.html')
+        self.ui.webEngineView.load(QUrl.fromLocalFile(html_file_path))
+        self.ui.webEngineView.setZoomFactor(8)
+        self.ui.actionShow_Bible.setText("Hide Bible")
         
         self.setWindowTitle("Open Source Bible System v0.0")
         self.ui.textEdit.setAcceptRichText = True
 
         # Connect signals
-        self.ui.textSize.valueChanged.connect(self.update_font_size)
+        self.ui.testSize.valueChanged.connect(self.update_font_size)
         self.ui.fontSelect.currentFontChanged.connect(self.update_font_family)
         self.ui.colorButton.clicked.connect(self.choose_color)
-
         self.ui.actionOpen.triggered.connect(self.open_file)
         self.ui.actionSave.triggered.connect(self.save_file)
+        self.ui.actionShow_Bible.triggered.connect(self.toggle_bible_window)
+        self.ui.bibleSizeUp.clicked.connect(lambda: self.update_bible_size(-1))
+        self.ui.bibleSizeDown.clicked.connect(lambda: self.update_bible_size(1))
+        self.ui.bibleSelection.currentTextChanged.connect(self.update_bible_version)
+        self.ui.bibleSelection.textActivated.connect(self.load_custom_bible)
+        
+    def update_bible_version(self):
+        if self.ui.bibleSelection.currentText() != "Custom HTML...":
+            self.ui.webEngineView.load(QUrl.fromLocalFile(os.path.abspath(self.bible_versions[self.ui.bibleSelection.currentText()])))
+            self.ui.webEngineView.setZoomFactor(8)
+    
+    def load_custom_bible(self):
+        if self.ui.bibleSelection.currentText() == "Custom HTML...":
+            file, _ = QFileDialog.getOpenFileName(self, "Open File", "", "HTML Files (*.html)")
+            self.ui.webEngineView.load(QUrl.fromLocalFile(file))
+            self.ui.webEngineView.setZoomFactor(8)
+    
+    def toggle_bible_window(self):
+        # Hides and shows the built-in Bible reader.
+        if self.ui.dockWidget_2.isHidden():
+            self.ui.dockWidget_2.show()
+            self.ui.actionShow_Bible.setText("Hide Bible")
+        else:
+            self.ui.dockWidget_2.hide()
+            self.ui.actionShow_Bible.setText("Show Bible")
+
+    def update_bible_size(self, zoom_value):
+        # Sets the scale of the built in Bible reader.
+        self.bible_size_value += zoom_value
+        self.ui.webEngineView.setZoomFactor(10 / self.bible_size_value)
 
     def save_file(self):
-        #Saves a File to a custom ".osbn" File.
+        # Saves a File to a custom ".osbn" File.
         options = QFileDialog.Option()
         filename, _ = QFileDialog.getSaveFileName(self, "Save File", "", "Open Source Bible Notes Files (*.osbn);;All Files (*)", options=options)
         if filename:
@@ -34,6 +77,7 @@ class MyGUI(QMainWindow):  # Main window setup
                f.write(self.ui.textEdit.toHtml())
 
     def open_file(self):
+        # Opens notes from a custom ".osbn" File.
         options = QFileDialog.Option()
         filename, _ = QFileDialog.getOpenFileName(self, "Open File", "", "Open Source Bible Notes Files (*.osbn);;All Files (*)", options=options)
         if filename:
@@ -55,7 +99,7 @@ class MyGUI(QMainWindow):  # Main window setup
 
         # Update font size in the UI
         font_size = char_format.fontPointSize()
-        self.ui.textSize.setValue(int(font_size) if font_size else 12)  # Set default if size isn't found
+        self.ui.testSize.setValue(int(font_size) if font_size else 12)  # Set default if size isn't found
 
         # Update color in the UI (use the color button's stylesheet)
         color = char_format.foreground().color()
@@ -64,7 +108,7 @@ class MyGUI(QMainWindow):  # Main window setup
 
     def update_font_size(self):
         # Update font size only
-        self.currentFormat.setFontPointSize(self.ui.textSize.value())
+        self.currentFormat.setFontPointSize(self.ui.testSize.value())
         self.ui.textEdit.setCurrentCharFormat(self.currentFormat)
 
     def update_font_family(self):
@@ -79,7 +123,7 @@ class MyGUI(QMainWindow):  # Main window setup
             self.currentFormat.setForeground(color)  # Update only color
             self.ui.textEdit.setCurrentCharFormat(self.currentFormat)
 
-            self.ui.colorButton.setStyleSheet(f"background-color: {color.name()};") #Update color button
+            self.ui.colorButton.setStyleSheet(f"background-color: {color.name()};") # Update color button
 
 def main():
     app = QApplication([])
--- Code/ui_main.py
+++ Code/ui_main.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'mainYwfztr.ui'
+## Form generated from reading UI file 'mainoyMcyf.ui'
 ##
 ## Created by: Qt User Interface Compiler version 6.8.0
 ##
@@ -16,16 +16,18 @@ from PySide6.QtGui import (QAction, QBrush, QColor, QConicalGradient,
     QIcon, QImage, QKeySequence, QLinearGradient,
     QPainter, QPalette, QPixmap, QRadialGradient,
     QTransform)
-from PySide6.QtWidgets import (QApplication, QFontComboBox, QGridLayout, QGroupBox,
-    QHBoxLayout, QLabel, QMainWindow, QMenu,
-    QMenuBar, QPushButton, QSizePolicy, QSpinBox,
-    QStatusBar, QTextEdit, QWidget)
+from PySide6.QtWebEngineWidgets import QWebEngineView
+from PySide6.QtWidgets import (QApplication, QComboBox, QDockWidget, QFontComboBox,
+    QGridLayout, QGroupBox, QHBoxLayout, QLabel,
+    QMainWindow, QMenu, QMenuBar, QPushButton,
+    QSizePolicy, QSpinBox, QStatusBar, QTextEdit,
+    QWidget)
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(569, 416)
+        MainWindow.resize(920, 481)
         MainWindow.setStyleSheet(u"")
         self.actionSave = QAction(MainWindow)
         self.actionSave.setObjectName(u"actionSave")
@@ -39,6 +41,8 @@ class Ui_MainWindow(object):
         self.action18px.setObjectName(u"action18px")
         self.action24px = QAction(MainWindow)
         self.action24px.setObjectName(u"action24px")
+        self.actionShow_Bible = QAction(MainWindow)
+        self.actionShow_Bible.setObjectName(u"actionShow_Bible")
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
         self.gridLayout_2 = QGridLayout(self.centralwidget)
@@ -63,10 +67,10 @@ class Ui_MainWindow(object):
 
         self.horizontalLayout.addWidget(self.label)
 
-        self.textSize = QSpinBox(self.groupBox)
-        self.textSize.setObjectName(u"textSize")
+        self.testSize = QSpinBox(self.groupBox)
+        self.testSize.setObjectName(u"testSize")
 
-        self.horizontalLayout.addWidget(self.textSize)
+        self.horizontalLayout.addWidget(self.testSize)
 
         self.label_3 = QLabel(self.groupBox)
         self.label_3.setObjectName(u"label_3")
@@ -89,18 +93,64 @@ class Ui_MainWindow(object):
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QMenuBar(MainWindow)
         self.menubar.setObjectName(u"menubar")
-        self.menubar.setGeometry(QRect(0, 0, 569, 33))
+        self.menubar.setGeometry(QRect(0, 0, 920, 33))
         self.menuFile = QMenu(self.menubar)
         self.menuFile.setObjectName(u"menuFile")
+        self.menuVeiw = QMenu(self.menubar)
+        self.menuVeiw.setObjectName(u"menuVeiw")
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QStatusBar(MainWindow)
         self.statusbar.setObjectName(u"statusbar")
         MainWindow.setStatusBar(self.statusbar)
+        self.dockWidget_2 = QDockWidget(MainWindow)
+        self.dockWidget_2.setObjectName(u"dockWidget_2")
+        self.dockWidgetContents_2 = QWidget()
+        self.dockWidgetContents_2.setObjectName(u"dockWidgetContents_2")
+        self.gridLayout = QGridLayout(self.dockWidgetContents_2)
+        self.gridLayout.setObjectName(u"gridLayout")
+        self.label_5 = QLabel(self.dockWidgetContents_2)
+        self.label_5.setObjectName(u"label_5")
+
+        self.gridLayout.addWidget(self.label_5, 0, 0, 1, 1)
+
+        self.bibleSelection = QComboBox(self.dockWidgetContents_2)
+        self.bibleSelection.addItem("")
+        self.bibleSelection.addItem("")
+        self.bibleSelection.addItem("")
+        self.bibleSelection.setObjectName(u"bibleSelection")
+
+        self.gridLayout.addWidget(self.bibleSelection, 0, 1, 1, 1)
+
+        self.label_4 = QLabel(self.dockWidgetContents_2)
+        self.label_4.setObjectName(u"label_4")
+
+        self.gridLayout.addWidget(self.label_4, 0, 2, 1, 1)
+
+        self.bibleSizeUp = QPushButton(self.dockWidgetContents_2)
+        self.bibleSizeUp.setObjectName(u"bibleSizeUp")
+
+        self.gridLayout.addWidget(self.bibleSizeUp, 0, 3, 1, 1)
+
+        self.bibleSizeDown = QPushButton(self.dockWidgetContents_2)
+        self.bibleSizeDown.setObjectName(u"bibleSizeDown")
+
+        self.gridLayout.addWidget(self.bibleSizeDown, 0, 4, 1, 1)
+
+        self.webEngineView = QWebEngineView(self.dockWidgetContents_2)
+        self.webEngineView.setObjectName(u"webEngineView")
+        self.webEngineView.setUrl(QUrl(u"about:blank"))
+
+        self.gridLayout.addWidget(self.webEngineView, 1, 0, 1, 5)
+
+        self.dockWidget_2.setWidget(self.dockWidgetContents_2)
+        MainWindow.addDockWidget(Qt.DockWidgetArea.LeftDockWidgetArea, self.dockWidget_2)
 
         self.menubar.addAction(self.menuFile.menuAction())
+        self.menubar.addAction(self.menuVeiw.menuAction())
         self.menuFile.addAction(self.actionOpen)
         self.menuFile.addAction(self.actionSave)
         self.menuFile.addAction(self.actionClose)
+        self.menuVeiw.addAction(self.actionShow_Bible)
 
         self.retranslateUi(MainWindow)
 
@@ -115,11 +165,22 @@ class Ui_MainWindow(object):
         self.action12px.setText(QCoreApplication.translate("MainWindow", u"12px", None))
         self.action18px.setText(QCoreApplication.translate("MainWindow", u"18px", None))
         self.action24px.setText(QCoreApplication.translate("MainWindow", u"24px", None))
+        self.actionShow_Bible.setText(QCoreApplication.translate("MainWindow", u"Show Bible", None))
         self.groupBox.setTitle(QCoreApplication.translate("MainWindow", u"Formating", None))
         self.label_2.setText(QCoreApplication.translate("MainWindow", u"Font:", None))
         self.label.setText(QCoreApplication.translate("MainWindow", u"Text Size:", None))
         self.label_3.setText(QCoreApplication.translate("MainWindow", u"Text Color:", None))
         self.colorButton.setText("")
         self.menuFile.setTitle(QCoreApplication.translate("MainWindow", u"File", None))
+        self.menuVeiw.setTitle(QCoreApplication.translate("MainWindow", u"View", None))
+        self.dockWidget_2.setWindowTitle(QCoreApplication.translate("MainWindow", u"Bible", None))
+        self.label_5.setText(QCoreApplication.translate("MainWindow", u"Version:", None))
+        self.bibleSelection.setItemText(0, QCoreApplication.translate("MainWindow", u"King James Version", None))
+        self.bibleSelection.setItemText(1, QCoreApplication.translate("MainWindow", u"American Standard Version", None))
+        self.bibleSelection.setItemText(2, QCoreApplication.translate("MainWindow", u"Custom HTML...", None))
+
+        self.label_4.setText(QCoreApplication.translate("MainWindow", u"Bible Size:", None))
+        self.bibleSizeUp.setText(QCoreApplication.translate("MainWindow", u"+", None))
+        self.bibleSizeDown.setText(QCoreApplication.translate("MainWindow", u"-", None))
     # retranslateUi
 
